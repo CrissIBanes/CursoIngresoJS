98b548046af588fcbeb585deb222ac5fc0bb9e31
CrissIBanes <60608629+CrissIBanes@users.noreply.github.com>
2020-02-03 12:34:17 -0300

    Add files via upload

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,9 @@
+/*Debemos lograr tomar un nombre con 'prompt'
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+  // var nombre
+   nombre = prompt ("Ingrese su nombre de usuario");
+   contraseña = prompt ("Ingrese su contraseña");
+}alert ("Usted inicio sesion");
+
