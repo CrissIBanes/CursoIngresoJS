c58e00f5e87cdba9df9216c653cd162772e6e6c4
CrissIBanes <bantonea@gmail.com>
2020-02-07 12:22:56 -0300

    clase 2

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,21 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	
+//Pido memoria para guardar la edad y el nombre.
+    var nombre; 
+
+    var edad; 
+
+    nombre = document.getElementById("elNombre").value;
+
+    edad = document.getElementById("laEdad").value;
+
+    alert("Usted se llama " + nombre + " Y tiene " + edad + " años."); 
+
+
+
+
+
+    
 }
 
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,29 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+//1
+var sueldo;
+var aumento; 
+var sueldofinal
+sueldo = document.getElementById("sueldo").value; 
+sueldo = parseInt(sueldo); 
+//2
+aumento = sueldo * 10 / 100;
+//3 
+sueldofinal = sueldo + aumento; 
+//4 
+document.getElementById("resultado").vaule = sueldofinal; 
+
+
+
+
+ 
+
+
+
+
+
+
 
 	
 }
