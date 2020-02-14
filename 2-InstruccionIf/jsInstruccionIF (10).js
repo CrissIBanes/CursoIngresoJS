3b7f9467204b94cbf7a2d1c4ec37fedfa2971842
CrissIBanes <60608629+CrissIBanes@users.noreply.github.com>
2020-02-14 12:16:53 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -2,5 +2,22 @@ function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
 	
+var numero; 
+var maximo = 10; 
+var minimo = 1; 
+
+numero =Math.floor(Math.random() * ((maximo + 1) - minimo) + minimo); 
+
+alert(numero); 
+
+if (numero >= 9){
+	alert ("EXCELENTE!!");
+} else if (numero >= 4 && numero <= 8) {
+alert ("Aprobado"); 
+} else {
+alert("Vamos, la proxima se puede!");
+
+}
+
 
 }//FIN DE LA FUNCIÓN
