fd7befb85317c27eb86c56e65ad0b5a81d54e929
alumno <alumno@utnfra.com.ar>
2020-02-21 12:19:44 -0300

    Clase 6

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,6 +3,18 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
+	var seguir = 'Si'; 
+
+	wwhile (seguir == 'Si') {
+		alert("Hola");
+
+		seguir = prompt("Quiere que lo salude de nuevo?");
+
+
+
+	}
+
+	
 
 
 
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -1,18 +1,35 @@
 function mostrar()
 {
-
+    var flag = 0;
 	var contador=0;
-	// declarar variables
-	
-	var respuesta='si';
+	var numero; 
+	var maximo;
+	var minimo; 
+	var respuesta = 's'; 
 
-	while(respuesta!='no')
-	{
-		
-	
+	do{
+	numero = parseInt(prompt("Ingrese un numero")); 
+	while (isNaN(numero)) { 
+    numero = parseInt(prompt("Esto no es un numero. Ingrese un numero")); 
+
 	}
+	if (flag == 0 || numero > maximo) {
+		maximo = numero;
+	}
+	if (flag == 0 || numero < minimo){
+		minimo = numero;
+		flag = 1; 
+	} 
+	respuesta = prompt("Quiere ingesar otro numero? "); 
+	}while(respuesta == 's'  ); 
+	
+	document.getElementById("maximo").value = maximo; 
+	document.getElementById("minimo").value = minimo; 
+
+	
+	
 
 
 
 
-}//FIN DE LA FUNCIÓN
+} //FIN DE LA FUNCIÓN
